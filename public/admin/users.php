a6d00833203a1947fb787453da3e3024bd2299d2
Dimas Febriyanto <dimassfeb@gmail.com>
2025-08-24 22:20:51 +0700

    fix users admin

--- public/admin/users.php
+++ public/admin/users.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/../src/db_connect.php';
+require_once __DIR__ . '/../../src/db_connect.php';
 
 // Check if user is logged in as admin
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
