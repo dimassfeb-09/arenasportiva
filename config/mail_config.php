f8c537042341a5dce714a3fa80ebb7f7bde22799
Dimas Febriyanto <dimassfeb@gmail.com>
2025-08-27 12:47:28 +0700

    fix mailer

--- config/mail_config.php
+++ config/mail_config.php
@@ -1,10 +1,14 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__ . '/../vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 $mail->Host       = 'smtp.gmail.com';
 $mail->SMTPAuth   = true;
-$mail->Username   = 'arenasportivamyid@gmail.com'; // email yang sudah dikonfigurasi
-$mail->Password   = 'kzjs eljg mzda ouia'; // app password yang sudah ada
+$mail->Username   = 'arenasportivamyid@gmail.com';
+$mail->Password   = 'kzjs eljg mzda ouia'; // app password
 $mail->SMTPSecure = 'tls';
 $mail->Port       = 587;
 $mail->setFrom('arenasportivamyid@gmail.com', 'Arena Sportiva');
